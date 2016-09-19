4bbe18c0449a1a88538fde3e62c7d4d782d056da
Royce Townsend <git@roycetownsend.com>
2016-09-20 02:51:54 +0800

    ...

--- snabbdom-app.js
+++ snabbdom-app.js
@@ -44,7 +44,7 @@ function isVacantRing(state, coord) {
 
 function validMoves(state) {
   var coord = state.event[2];
-  var arr = [];
+  var moves = set();
   for (var i = 0; i < directions.length; i++) {
     var step = steps[directions[i]];
     var path = [];
@@ -54,10 +54,10 @@ function validMoves(state) {
       next = step(next);
     }
     if (path.length > 0) {
-      arr.push(path.pop());
+      moves.add(path.pop());
     }
   }
-  return arr;
+  return moves;
 }
 
 function ringCanBeMoved(state, coord) {
@@ -91,9 +91,18 @@ function ringCanBeMoved(state, coord) {
   return foundAdjacentEmpties;
 }
 
+function sameCoord(left, right) {
+  return left[0] === right[0] && left[1] === right[1];
+}
+
 function ringSelected(state, coord) {
   return state.event[0] === "ring-selected"
-    && JSON.stringify(state.event[2]) === JSON.stringify(coord)
+    && sameCoord(state.event[2], coord)
+}
+
+function ringMovedLastTurn(state, coord) {
+  return state.last_ring_move
+    && sameCoord(state.last_ring_move[2], coord)
 }
 
 var initial = function() {
@@ -101,18 +110,13 @@ var initial = function() {
     rings: set(
       /*  */ [1,0], [2,0], [3,0],
       /**/[0,1], [1,1], [2,1], [3,1],
-      // /**/       [1,1], [2,1], [3,1],
       [0,2], [1,2], [2,2], [3,2], [4,2],
-      // [0,2], [1,2],        [3,2], [4,2],
       /**/[0,3], [1,3], [2,3], [3,3],
       /*  */ [1,4], [2,4], [3,4],
     ),
     red: set([1,0], [4,2], [1,4]),
     blue: set([3,0], [0,2], [3,4]),
     event: ["turn-began", "red"]
-    // event: ["marble-selected", "red", [1,0]]
-    // event: ["marble-moved", "red", [1,0]]
-    // event: ["ring-selected", "red", [1,0]]
   };
 };
 var state = initial();
@@ -123,12 +127,22 @@ function render() {
 }
 
 function handler(e, color, coord) {
+  color = color || state.event[1];
   console.log(e, color, coord);
   if (e === "marble-moved") {
     state[color].rm(state.event[2]);
     state[color].add(coord);
   }
-  state.event = [e, color, coord];
+  if (e === "ring-moved") {
+    state.rings.rm(state.event[2]);
+    state.rings.add(coord);
+    state.last_ring_move = [color, state.event[2], coord];
+
+    state.event = ["turn-began", color === "red" ? "blue" : "red"]
+  }
+  else {
+    state.event = [e, color, coord];
+  }
   render();
 }
 
@@ -142,11 +156,12 @@ function ring(props) {
         cy: cy(props.coord),
         r: 18,
         fill: "transparent",
-        stroke: props.highlight ? "#bbb" : "#333",
-        'stroke-width': props.highlight ? "5px" : "5px",
-        'stroke-dasharray': props.highlight ? "5,5" : "0"
+        stroke: props.highlight ? "#bbb" :
+          (props.color ? lighterColor[props.color] : "#333"),
+        'stroke-width': props.feint ? "1px" : "5px",
+        'stroke-dasharray': (props.highlight || props.feint) ? "5,5" : "0"
       },
-      on: props.event ? {click: [handler, props.event, props.color, props.coord]} : null
+      on: props.event ? {click: [handler, props.event, undefined, props.coord]} : null
     }
   );
 }
@@ -167,47 +182,76 @@ function candidateRings(state) {
   if (state.event[0] === "marble-moved"
     || state.event[0] === "ring-selected") {
     state.rings.forEach(function (coord) {
-      if (ringCanBeMoved(state, coord) && !ringSelected(state, coord)) {
+      if (ringCanBeMoved(state, coord)
+          && !ringSelected(state, coord)
+          && !ringMovedLastTurn(state, coord)) {
         r.push(ring({
           coord:coord,
           highlight:true,
           event:"ring-selected"}));
       }
     });
-    // r.push(ring({coord:[0,0], highlight:true}));
   }
   return r;
 }
 
+function validRingDest(state, ringToBeMoved) {
+  var _once = set();
+  var _twice = set();
+  var otherRings = set(state.rings);
+  otherRings.rm(ringToBeMoved);
+  otherRings.forEach(function (coord) {
+    adjacents(state, coord).forEach(function (currentCoord) {
+      if (!otherRings.has(currentCoord)) {
+        if(_once.add(currentCoord)) {
+          _twice.add(currentCoord);
+        }
+      }
+    });
+  });
+  _twice.rm(ringToBeMoved);
+  if (state.last_ring_move) {
+    _twice.rm(state.last_ring_move[1])
+  }
+  return _twice;
+}
+
 function potentialRings(state) {
   var r = [];
-  var ringToBeMoved = state.event[2];
-  var _once = new set();
-  var _twice = new set();
   if (state.event[0] === "ring-selected") {
-    var otherRings = set(state.rings);
-    otherRings.rm(ringToBeMoved);
-    otherRings.forEach(function (coord) {
-      adjacents(state, coord).forEach(function (currentCoord) {
-        if (!otherRings.has(currentCoord)) {
-          if(_once.add(currentCoord)) {
-            _twice.add(currentCoord);
-          }
-        }
-      });
-    });
-    _twice.rm(ringToBeMoved);
-    _twice.forEach(function (coord) {
-      r.push(ring({coord:coord, highlight:true}));
+    validRingDest(state, state.event[2]).forEach(function (coord) {
+      r.push(ring({
+        coord:coord,
+        highlight:true,
+        event:"ring-moved"
+      }));
     })
   }
   return r;
 }
 
+function lastRingMove(state) {
+  if (state.last_ring_move) {
+    return [
+      ring({
+        coord:state.last_ring_move[1],
+        color:state.last_ring_move[0],
+        feint:true,
+      }),
+      ring({
+        coord:state.last_ring_move[2],
+        color:state.last_ring_move[0],
+        feint:true,
+      })
+    ];
+  }
+  return [];
+}
+
 
 var lighterColor = {
   "red": "pink",
-  "blue": "light-blue"
+  "blue": "lightblue"
 }
 function marble(props) {
   return h(
@@ -246,15 +290,14 @@ function potentialMarbles(state) {
   var r = [];
   if (state.event[0] === "marble-selected") {
     var color = state.event[1];
-    var p = validMoves(state);
-    for (var i = 0; i < p.length; i++) {
+    validMoves(state).forEach(function (coord) {
       r.push(marble({
         color:color,
         lighten:true,
-        coord:p[i],
+        coord:coord,
         event: "marble-moved"
       }));
-    }
+    });
   }
   return r;
 }
@@ -262,13 +305,14 @@ function potentialMarbles(state) {
 
 view = function view(state) {
   return h('div', [
-    h('svg', {attrs: {width: 300, height: 300}}, flatten([
+    h('svg', {attrs: {width: 400, height: 400}}, flatten([
       rings(state),
+      candidateRings(state),
+      potentialRings(state),
+      lastRingMove(state),
       marbles(state, "red"),
       marbles(state, "blue"),
       potentialMarbles(state),
-      candidateRings(state),
-      potentialRings(state),
     ])
     )
   ]);
